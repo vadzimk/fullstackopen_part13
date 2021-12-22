235fb7d56586de1979d868c45d955c902ec4939a
vadzim <vadzimk@hotmail.com>
2021-12-22 14:04:05 -0800

    amend2 task 13.5

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -3,7 +3,7 @@ const blogsRouter = express.Router()
 
 import {Blog} from '../models/index.js';
 
-blogsRouter.get('/api/blogs', async(req, res) => {
+blogsRouter.get('/', async(req, res) => {
   try {
     const blogs = await Blog.findAll()
     res.json(blogs)
@@ -12,7 +12,7 @@ blogsRouter.get('/api/blogs', async(req, res) => {
   }
 });
 
-blogsRouter.post('/api/blogs', async (req, res) => {
+blogsRouter.post('/', async (req, res) => {
   try{
     const note = await Blog.create(req.body)
     res.json(note)
@@ -21,7 +21,7 @@ blogsRouter.post('/api/blogs', async (req, res) => {
   }
 });
 
-blogsRouter.delete('/api/blogs/:id', async (req, res) => {
+blogsRouter.delete('/:id', async (req, res) => {
   const success = await Blog.destroy({where:{id:req.params.id}})
   if(success){
     res.status(204).end()
