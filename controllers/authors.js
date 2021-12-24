31f225d2dda6136c7981cc52ee813a0892ec0446
vadzim <vadzimk@hotmail.com>
2021-12-24 13:04:31 -0800

    task 13.16 with group by

--- controllers/authors.js
+++ controllers/authors.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import {Blog, User} from '../models/index.js';
-import 'express-async-errors'  // error handling middleware
+import 'express-async-errors'
+import {fn, col, literal} from 'sequelize';  // error handling middleware
 
 const authorsRouter = express.Router()
 //     author: "Jami Kousa",
@@ -8,23 +9,24 @@ const authorsRouter = express.Router()
 //     likes: "10"
 // select name from user
 authorsRouter.get('/', async(req,res)=>{
-  const result = await User.findAll({
-    attributes: [['name', 'author']],
-    include: {
-      model: Blog,
-      attributes: ['likes']
-    }
+  const result = await Blog.findAll({
+    attributes: [
+      'author',
+      [fn('COUNT', col('id')), 'articles'],
+      [fn('SUM', col('likes')), 'likes']],
+    group: ['author'],
+    order: literal('"articles" DESC') // literally plug in sql
   })
 
-  console.log(result.map(i=>i.toJSON()))
-  const authors = result.map(i=>{
-    const item = i.toJSON()
-    return {
-    author: item.author,
-    articles: item.blogs.length,
-    likes: item.blogs.reduce((acc, e)=> acc + e.likes, 0)
-  }})
-  res.json(authors)
+  // console.log(result.map(i=>i.toJSON()))
+  // const authors = result.map(i=>{
+  //   const item = i.toJSON()
+  //   return {
+  //   author: item.author,
+  //   articles: item.blogs.length,
+  //   likes: item.blogs.reduce((acc, e)=> acc + e.likes, 0)
+  // }})
+  res.json(result)
 
 
 
